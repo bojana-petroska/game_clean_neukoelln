a5848d65fa1fad10fb1e98aee1b6c67d2d72c8f1
bojana-petroska <bojana.petroska@yahoo.com>
2023-03-31 17:07:00 +0200

    still first try

---
+++ game.js
@@ -0,0 +1,18 @@
+class Game {
+    constructor() {
+        this.background = new Background()
+        this.player = new Player()
+        this.backgroundImages
+        this.playerImage
+        this.obstacles = []
+        this.garbage
+        this.plasticBag
+        this.mouse
+    }
+
+    preload() {
+        this.backgroundImages = [
+            
+        ]
+    }
+}
---
+++ main.js
@@ -0,0 +1,20 @@
+//const game = new Game()
+
+// Load game assets
+function preload() {
+	//game.preload()
+}
+
+// Setup game
+function setup() {
+	createCanvas(600, 600)
+    console.log('start')
+}
+
+function draw() {
+	//game.draw()
+}
+
+function keyPressed() {
+
+}
